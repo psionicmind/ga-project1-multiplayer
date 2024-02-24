59215da79b07a7bfa168b5537c9fc635d103c58d
ken-chan <work.kenchan@gmail.com>
2024-02-24 18:48:15 +0800

    removed testing package and reset the game instantly after new side by side player is added

--- script.js
+++ script.js
@@ -104,6 +104,9 @@ function addPlayer(username, IsMainPlayer){
         UiCreateDisc(username);                // ui
         loadAllDiscToTower1(username);         // ui
     }
+    else{
+        resetGame(username)
+    }
 
 }
 
